fb1b220dad17c1d39eaba4faddf09cb4d4b61a2e
Korwin <korwin-bieniek@wp.pl>
2021-12-09 14:50:23 +0100

    This version helps the player by displaying the first 3 letters of the correct word and hiding the lest of the letters with '-' sign.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -2,9 +2,14 @@ import random
 
 # Write your code here
 print("H A N G M A N")
-player_word = input("Guess the word: ")
 list_of_words = ['python', 'java', 'kotlin', 'javascript']
 correct_word = list_of_words[random.randint(0, len(list_of_words) - 1)]
+print(correct_word[:3], end='')
+for i in range(len(correct_word) - 3):
+    print('-', end='')
+print()
+player_word = input("Guess the word: ")
+
 if player_word == correct_word:
     print("You survived!")
 else:

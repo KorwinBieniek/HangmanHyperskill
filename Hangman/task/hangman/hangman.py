384f5381287bc4dc844b2cc500488933c2447f6e
Korwin <korwin-bieniek@wp.pl>
2021-12-09 14:40:47 +0100

    This version has the modifier that the word is randomly chosen from the list of words.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,7 +1,11 @@
+import random
+
 # Write your code here
 print("H A N G M A N")
-correct_word = input("Guess the word: ")
-if correct_word == "python":
+player_word = input("Guess the word: ")
+list_of_words = ['python', 'java', 'kotlin', 'javascript']
+correct_word = list_of_words[random.randint(0, len(list_of_words) - 1)]
+if player_word == correct_word:
     print("You survived!")
 else:
     print("You lost!")

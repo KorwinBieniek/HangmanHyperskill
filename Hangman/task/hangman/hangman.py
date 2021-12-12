e11b7ee7a15a4c59779cf585c4e60dab5738d12c
Korwin <korwin-bieniek@wp.pl>
2021-12-12 06:17:28 +0100

    6/8 stage to give the user 8 chances to make a mistake and give them a condition to win the game.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,7 +5,7 @@ lives = 8
 print("H A N G M A N\n")
 used_letters = set()
 alphabet = set(string.ascii_lowercase)
-list_of_words = ['python', 'java', 'kotlin']
+list_of_words = ['python', 'java', 'kotlin', 'javascript']
 correct_word = list_of_words[random.randint(0, len(list_of_words) - 1)]
 word_letters = set(correct_word)
 random_letter = set()
@@ -14,37 +14,33 @@ random_letter = set()
 
 #word_letters = word_letters.union(word_letters, random_letter)
 
-while lives > 0:
+while len(word_letters) > 0 and lives > 0:
     # print("You have", lives, "lives left and you have used these letters: ", ' '.join(used_letters))
 
     # the current word
     word_list = [letter if (letter in random_letter or letter in used_letters) else '-' for letter in correct_word]
+    print('\n')
     print(''.join(word_list))
 
     user_letter = input('Input a letter: ').lower()
 
     if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
+
         if user_letter in word_letters:
+            used_letters.add(user_letter)
             word_letters.remove(user_letter)
             print('\n')
         else:
             lives -= 1
-            print('That letter doesn\'t appear in the word\n')
+            print('That letter doesn\'t appear in the word')
     elif user_letter in used_letters:
-        #print('\nYou have already used that letter. Please try again.')
-        print('\n')
+        print('No improvements')
+        #print('\n')
         lives -= 1
     #else:
         #print("\nThat is not a valid letter")
 
-'''if lives == 0:
-    print('\nYou died, sorry. The word was', correct_word)
+if lives == 0:
+    print('You lost!')
 else:
-    word_list = [letter if (letter in random_letter or letter in used_letters) else '-' for letter in correct_word]
-    print(''.join(word_list))
-    print('\n')
-    #print("\nYou won! You guess the word", correct_word)'''
-
-print("Thanks for playing!")
-print("We'll see how well you did in the next stage")
+    print(f'{correct_word}\nYou guessed the word!\nYou survived!')

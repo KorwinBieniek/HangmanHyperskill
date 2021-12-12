e3b9da87f2dd8723a22605000afcbe03fa52c416
Korwin <korwin-bieniek@wp.pl>
2021-12-12 06:38:39 +0100

    Error handling

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -4,15 +4,16 @@ import string
 lives = 8
 print("H A N G M A N\n")
 used_letters = set()
-alphabet = set(string.ascii_lowercase)
+alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z'}
 list_of_words = ['python', 'java', 'kotlin', 'javascript']
 correct_word = list_of_words[random.randint(0, len(list_of_words) - 1)]
 word_letters = set(correct_word)
 random_letter = set()
-#for i in range(3):
-    #random_letter.add(random.choice(correct_word))
+# for i in range(3):
+# random_letter.add(random.choice(correct_word))
 
-#word_letters = word_letters.union(word_letters, random_letter)
+# word_letters = word_letters.union(word_letters, random_letter)
 
 while len(word_letters) > 0 and lives > 0:
     # print("You have", lives, "lives left and you have used these letters: ", ' '.join(used_letters))
@@ -22,23 +23,31 @@ while len(word_letters) > 0 and lives > 0:
     print('\n')
     print(''.join(word_list))
 
-    user_letter = input('Input a letter: ').lower()
+    user_letter = input('Input a letter: ')
 
-    if user_letter in alphabet - used_letters:
+   # if user_letter in {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                       #'T', 'u',
+                       #'V', 'W', 'X', 'Y', 'Z'}:
+    if len(user_letter) != 1:
+        print('You should input a single letter')
+    elif user_letter not in alphabet:
+        print('Please enter a lowercase English letter')
 
+    elif user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
         if user_letter in word_letters:
-            used_letters.add(user_letter)
             word_letters.remove(user_letter)
+            # used_letters.add(user_letter)
             print('\n')
         else:
             lives -= 1
             print('That letter doesn\'t appear in the word')
+            if lives == 1:
+                print('That letter doesn\'t appear in the word')
     elif user_letter in used_letters:
-        print('No improvements')
-        #print('\n')
-        lives -= 1
-    #else:
-        #print("\nThat is not a valid letter")
+        print('You\'ve already guessed this letter')
+        # print('\n')
+        # lives -= 1
 
 if lives == 0:
     print('You lost!')

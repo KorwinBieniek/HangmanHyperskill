8a29a6a94f509de8e6b4cd19bb2fa3f017f38fc1
Korwin <korwin-bieniek@wp.pl>
2021-12-14 21:06:25 +0100

    Reformatted the code for the game. The code is now separated to the functions, name of variables was corrected. The proper main call has been used.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,63 +1,61 @@
 import random
-import string
 
-lives = 8
-print("H A N G M A N")
-used_letters = set()
-alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+ALPHABET = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z'}
-list_of_words = ['python', 'java', 'kotlin', 'javascript']
-correct_word = list_of_words[random.randint(0, len(list_of_words) - 1)]
-word_letters = set(correct_word)
-random_letter = set()
-# for i in range(3):
-# random_letter.add(random.choice(correct_word))
+LIST_OF_WORDS = ['python', 'java', 'kotlin', 'javascript']
+CORRECT_WORD = LIST_OF_WORDS[random.randint(0, len(LIST_OF_WORDS) - 1)]
 
-# word_letters = word_letters.union(word_letters, random_letter)
 
-correct = False
-while not correct:
-    choice = input('Type "play" to play the game, "exit" to quit: ')
-    if choice == 'exit':
-        exit(0)
-    elif choice == 'play':
-        correct = True
-        while len(word_letters) > 0 and lives > 0:
-            # print("You have", lives, "lives left and you have used these letters: ", ' '.join(used_letters))
+def start_hangman():
+    lives = 8
+    word_letters = set(CORRECT_WORD)
+    used_letters = set()
 
-            # the current word
-            word_list = [letter if (letter in random_letter or letter in used_letters) else '-' for letter in
-                         correct_word]
-            print('\n')
-            print(''.join(word_list))
+    while len(word_letters) > 0 and lives > 0:
+        print("You have", lives, "lives left and you have used these letters: ", ' '.join(used_letters))
 
-            user_letter = input('Input a letter: ')
+        # the current word
+        word_list = [letter if (letter in used_letters) else '-' for letter in
+                     CORRECT_WORD]
+        print('____________WORD_______________\n')
+        print(''.join(word_list))
+        print('\n___________________________')
 
-            # if user_letter in {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-            # 'T', 'u',
-            # 'V', 'W', 'X', 'Y', 'Z'}:
-            if len(user_letter) != 1:
-                print('You should input a single letter')
-            elif user_letter not in alphabet:
-                print('Please enter a lowercase English letter')
+        user_letter = input('Input a letter: ')
 
-            elif user_letter in alphabet - used_letters:
-                used_letters.add(user_letter)
-                if user_letter in word_letters:
-                    word_letters.remove(user_letter)
-                    # used_letters.add(user_letter)
-                    print('\n')
-                else:
-                    lives -= 1
+        if len(user_letter) != 1:
+            print('You should input a single letter')
+        elif user_letter not in ALPHABET:
+            print('Please enter a lowercase English letter')
+
+        elif user_letter in ALPHABET - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives -= 1
+                print('That letter doesn\'t appear in the word')
+                if lives == 1:
                     print('That letter doesn\'t appear in the word')
-                    if lives == 1:
-                        print('That letter doesn\'t appear in the word')
-            elif user_letter in used_letters:
-                print('You\'ve already guessed this letter')
-                # print('\n')
-                # lives -= 1
+        elif user_letter in used_letters:
+            print('You\'ve already guessed this letter')
 
-        if lives == 0:
-            print('You lost!')
-        else:
-            print(f'{correct_word}\nYou guessed the word!\nYou survived!')
+    if lives == 0:
+        print('You lost!')
+    else:
+        print(f'{CORRECT_WORD}\nYou guessed the word!\nYou survived!')
+
+
+def menu(correct):
+    while not correct:
+        print("H A N G M A N")
+        choice = input('Type "play" to play the game, "exit" to quit: ').upper()
+        if choice == 'EXIT':
+            exit(0)
+        elif choice == 'PLAY':
+            correct = True
+            start_hangman()
+
+
+if __name__ == '__main__':
+    menu(False)
